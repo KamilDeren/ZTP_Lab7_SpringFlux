2a6eeb8d9fa6296edb8b2e802563aaabc885834e
Kamil Dereń <91948022+KamilDeren@users.noreply.github.com>
2024-05-21 08:40:57 +0200

    Add files via upload

---
+++ ztplab7/src/main/java/pl/kamilderen/ztplab7/Ztplab7Application.java
@@ -0,0 +1,13 @@
+package pl.kamilderen.ztplab7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ztplab7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ztplab7Application.class, args);
+	}
+
+}
---
+++ ztplab7/src/test/java/pl/kamilderen/ztplab7/Ztplab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kamilderen.ztplab7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ztplab7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
